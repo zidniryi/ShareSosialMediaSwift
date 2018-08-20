87b9b1c0296bf7d4737e2231521ed7722eda6b7b
zidniryi <zidnirydwan@gmail.com>
2018-08-20 13:56:00 +0700

    Initial Commit

---
+++ Share Sosial Media/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Share Sosial Media
+//
+//  Created by hint on 20/08/18.
+//  Copyright © 2018 ZidniRyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
